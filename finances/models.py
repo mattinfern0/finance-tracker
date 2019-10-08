4789a8f97acf03267a98203c613ce24b60c80be0
Matthew Fernandez <mattfer98@gmail.com>
2019-10-07 18:50:36 -0700

    Basic django rest api for transactions

--- finances/models.py
+++ finances/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Transaction(models.Model):
-    title = models.CharField()
-    amount = models.DecimalField()
+    title = models.CharField(max_length=200)
+    amount = models.DecimalField(decimal_places=2, max_digits=8)
     date = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- finances/views.py
+++ finances/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from finances.serializers import TransactionSerializer
+from finances.models import Transaction
 
 # Create your views here.
+class TransactionViewSet(viewsets.ModelViewSet):
+    """ 
+    Get/edit transactions
+    """
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer

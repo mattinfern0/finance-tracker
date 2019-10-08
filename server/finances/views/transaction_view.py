c8379bd896d62735da5170e04a38eed16deb3f6e
Matthew Fernandez <mattfer98@gmail.com>
2019-10-08 16:17:59 -0700

    add transactions

--- server/finances/views/transaction_view.py
+++ server/finances/views/transaction_view.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.response import Response
 
 from finances.serializers import TransactionSerializer
 from finances.models import Transaction
+
+from rest_framework.status import (
+    HTTP_201_CREATED
+)
 
 class TransactionViewSet(viewsets.ModelViewSet):
     """ 
@@ -17,4 +22,19 @@ class TransactionViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         userId = self.request.session.get('user_id')
-        return self.queryset.filter(id=userId)
+        return self.queryset.filter(user=userId)
+
+    def create(self, request):
+        data = request.data
+
+        # Insert session user id into data before validating
+        data['user'] = self.request.session.get('user_id')
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=HTTP_201_CREATED, headers=headers)
+
+    def update(self, request):
+        print('Update request')
